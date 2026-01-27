78eeb4de8904ae8c6b56570b62ec37a2cea619e1
Ermira Krasniqi <krasniqi.2112344@studenti.uniroma1.it>
2026-01-27 16:32:37 +0100

    CRUD application in React (5)

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,10 +1,14 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Box, Container, CssBaseline} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react"
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 //react component is just a JavaScript function that returns something
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined> (undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
@@ -13,19 +17,60 @@ function App() {
     return () => {}
   }, [])
 
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined); 
+  }
+
+  const handleOpenForm = (id?: string) => {
+    if (id) handleSelectActivity(id);
+    else handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  const handleFormClose = () => {
+    setEditMode(false);
+  }
+
+  const handleSubmitForm = (activity: Activity) => {
+    if (activity.id) {
+    setSelectedActivity(activity);
+    setActivities(activities.map(x => x.id === activity.id ? activity : x))
+  } else {
+    const newActivity = {...activity, id: activities.length.toString()}
+    setSelectedActivity(newActivity);
+    setActivities([...activities, newActivity]);
+  }
+    setEditMode(false);
+}
+
+  const handleDelete = (id: string) => {
+    setActivities(activities.filter(x => x.id === id))
+  }
 
   return (
-    <>
-    <Typography variant='h3'>Reactivities</Typography>
-    <List>
-      {activities.map((activity) => (
-        <ListItem key={activity.id}>
-          <ListItemText>{activity.title}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
-    </>
+    <Box sx={{bgcolor: '#eeeeee'}}>
+      <CssBaseline />
+      <NavBar openForm={handleOpenForm}/>
+      <Container maxWidth='xl' sx={{mt: 3}}>
+        <ActivityDashboard 
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          openForm={handleOpenForm}
+          closeForm={handleFormClose}
+          submitForm={handleSubmitForm}
+          deleteActivity={handleDelete}
+        />
+      </Container>
+      
+      </Box>
   )
 }
 
-export default App
+export default App;
